1827757f343d3c0c5752ad898bb89f90e82d564e
JohannesSchaefers <info@iconicity.de>
2025-09-17 23:31:04 +0200

    Postergirls

--- fresh-project-sse/routes/index.tsx
+++ fresh-project-sse/routes/index.tsx
@@ -5,7 +5,7 @@ import Messages from "../islands/Messages.tsx";
 export default function Home() {
   return (
     <main style={{ padding: "1rem", fontFamily: "Arial, sans-serif" }}>
-      <h1>Live Messages</h1>
+      <h1>Live Messages Live Messages</h1>
       <Messages />
     </main>
   );
